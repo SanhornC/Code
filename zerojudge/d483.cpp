6b0a346a649d6b96b76b8710873bd52ebeaa281c
Sanhorn <sanhornchen@pacificamerican.org>
2020-08-24 12:33:35 +0800

    Create d483.cpp
    hello world

---
+++ zerojudge/d483.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  d483.cpp
+//
+//  Created by Sanhorn on 2020/8/24.
+//  Copyright © 2020 Sanhorn. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    return 0;
+}
