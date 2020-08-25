92b173bea81fed74cc4db9ac72b6ee6b49c9b2d2
Sanhorn <sanhornchen@pacificamerican.org>
2020-08-25 09:15:08 +0800

    Create a001.cpp

---
+++ zerojudge/a001.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a001.cpp
+//
+//  Created by Sanhorn on 2020/8/25.
+//  Copyright © 2020 Sanhorn. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    string a; cin >> a;
+    cout << "hello, " << a << endl;
+    return 0;
+}
