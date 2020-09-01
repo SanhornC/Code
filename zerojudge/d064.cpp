7eb643bf608411f387eeec236cb9ba8ef5396e55
Sanhorn <sanhornchen@pacificamerican.org>
2020-09-01 14:39:40 +0800

    Create d064.cpp

---
+++ zerojudge/d064.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  d064.cpp
+//
+//  Created by Sanhorn on 2020/9/1.
+//  Copyright © 2020 Sanhorn. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int a; cin >> a;
+    if (a%2 == 0)
+        cout << "Even";
+    else
+        cout << "Odd";
+    return 0;
+}
