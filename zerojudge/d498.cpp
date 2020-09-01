d39050c0d074d046111ccb764246d2049a655974
Sanhorn <sanhornchen@pacificamerican.org>
2020-09-01 14:49:38 +0800

    Create d498.cpp

---
+++ zerojudge/d498.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  d498.cpp
+//
+//  Created by Sanhorn on 2020/9/1.
+//  Copyright © 2020 Sanhorn. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int n; cin >> n;
+    for (int i=0; i<n; i++)
+        cout << "I don't say swear words!" << '\n';
+    return 0;
+}
